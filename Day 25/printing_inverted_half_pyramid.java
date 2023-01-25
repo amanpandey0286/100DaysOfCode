a38bf0554c07b683e5214eacd65da6c5344db37d
Aman Pandey <80692326+amanpandey0286@users.noreply.github.com>
2023-01-25 09:18:15 +0530

    Update printing_inverted_half_pyramid.java

--- Day 25/printing_inverted_half_pyramid.java
+++ Day 25/printing_inverted_half_pyramid.java
@@ -5,17 +5,19 @@ public static void main(String[] args) {
         for(int i = 1; i<=n; i++){
             //for loop to print extra space in row.
             for(int j1 = 1; j1 <= (n-i);j1++){
-                System.out.print("  ");
+                System.out.print(" ");
             }
             // for loop to print stars in row.
             for(int j2 = 1; j2<=(i); j2++){
-                System.out.print("* ");
+                System.out.print("*");
             }
             //println to change the line.
             System.out.println();
         }
     }
 
+// The variable initiated in a loop stays in the memory only till the loop is running, oncce the loop is finished the variable will be also remove from the memory.
+// hence in the place of j1 and j2 we can use only j as well.
 
 
 /*  To print 
