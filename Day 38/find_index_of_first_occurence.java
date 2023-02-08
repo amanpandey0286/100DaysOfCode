d15922f14a9cd3dc4f88a218e16f7aad082c552f
Aman Pandey <80692326+amanpandey0286@users.noreply.github.com>
2023-02-08 23:30:40 +0530

    Create find_index_of_first_occurence.java

---
+++ Day 38/find_index_of_first_occurence.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if (haystack.length() < needle.length()) return -1;
+        for (int i = 0; i < haystack.length(); i++) {
+            if (haystack.charAt(i) == needle.charAt(0)) {
+                for (int j = 0; i + j < haystack.length() && j < needle.length(); j++) {
+                    if (haystack.charAt(i + j) != needle.charAt(j)) {
+                        break;
+                    }
+                    if (j == needle.length()-1) {
+                        return i;
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+}
+
+
+//https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
