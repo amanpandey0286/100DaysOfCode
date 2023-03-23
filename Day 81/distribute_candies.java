97ec49bf0b2110a2f7f2c97ae745b224964a6c7d
Aman Pandey <80692326+amanpandey0286@users.noreply.github.com>
2023-03-23 20:51:55 +0530

    Create distribute_candies.java

---
+++ Day 81/distribute_candies.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        Set<Integer> typeCandySet = new HashSet<>();
+        int halfLength = candyType.length/2;
+
+        for (int c : candyType) {
+            if (typeCandySet.size() >= halfLength)
+                return halfLength;
+            typeCandySet.add(c);
+        }
+        return Math.min(typeCandySet.size(), halfLength);
+    }
+}
+
+
+//https://leetcode.com/problems/distribute-candies/description/
