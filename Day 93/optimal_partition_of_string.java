a23c7788df2215f078896111309f5bff7fe1b24f
Aman Pandey <80692326+amanpandey0286@users.noreply.github.com>
2023-04-04 23:46:24 +0530

    Create optimal_partition_of_string.java

---
+++ Day 93/optimal_partition_of_string.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int partitionString(String s) {
+        int subs = 1;
+        Set<Character> charSet = new HashSet<>();
+        for(char ch: s.toCharArray()){
+            if(!charSet.contains(ch)){
+                charSet.add(ch);
+                continue;
+            }
+            subs++;
+            charSet.clear();
+            charSet.add(ch);
+        }
+        return subs;
+    }
+}
+
+//https://leetcode.com/problems/optimal-partition-of-string/description/
