e4eeb1e0472fe8b1eef20b37ce9b5187eb7e840d
Aman Pandey <80692326+amanpandey0286@users.noreply.github.com>
2023-04-06 23:43:13 +0530

    Create convert_binary_to_int_in_linkedList.java

---
+++ Day 95/convert_binary_to_int_in_linkedList.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int getDecimalValue(ListNode head) {
+        int ans =0;
+        ListNode temp = head;
+        while(temp != null){
+            ans = ans*2;
+            ans = ans + temp.val;
+            temp = temp.next;
+        }
+        return ans;
+    }
+}
+
+//https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/description/
